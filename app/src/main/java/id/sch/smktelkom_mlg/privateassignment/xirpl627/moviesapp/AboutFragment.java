61e18acb5c9d7e01ec98a7646b36753416b614a0
farras13 <farrasgopal13@gmail.com>
2017-05-16 07:55:41 +0700

    Final commit 1.1

--- app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl627/moviesapp/AboutFragment.java
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl627/moviesapp/AboutFragment.java
@@ -1,4 +1,4 @@
-package layout;
+package id.sch.smktelkom_mlg.privateassignment.xirpl627.moviesapp;
 
 
 import android.os.Bundle;
@@ -6,8 +6,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import id.sch.smktelkom_mlg.privateassignment.xirpl627.moviesapp.R;
 
 /**
  * A simple {@link Fragment} subclass.
